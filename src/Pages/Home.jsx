72e30de0296077df850ed425377c162e22526d61
Nawal-Mohamed <teamwork980@gmail.com>
2025-05-19 18:33:38 +0300

    navbar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "../Common component/Navbar";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
