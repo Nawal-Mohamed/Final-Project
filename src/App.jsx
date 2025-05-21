93378a8d5dd9f24a047f1250ef4b63d1a606b300
esraa181175 <esraahassan146@gamil.com>
2025-05-21 18:25:58 +0300

    push components footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./Pages/Home";
+import FooterMain from "./Common component/Footer/FooterMain";
 
 export default function App() {
   return (
     <div>
       <Home />
+      <FooterMain />
     </div>
   );
 }
