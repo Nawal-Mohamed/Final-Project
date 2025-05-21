af0a16718934170ed88678f8849ccd008a5cb576
Nawal-Mohamed <teamwork980@gmail.com>
2025-05-21 22:18:52 +0300

    style edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Common component/Navbar";
+import Banner from "../component/Banner";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 }
